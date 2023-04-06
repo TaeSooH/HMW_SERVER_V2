f812bd7fa2661845b94ba5da1bcede35da574f6e
soohyang136 <jsh515228@gmail.com>
2023-04-06 17:26:14 +0900

    init

---
+++ src/main/java/TaeSuH/HMW/HmwApplication.java
@@ -0,0 +1,13 @@
+package TaeSuH.HMW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TaeSuH/HMW/HmwApplicationTests.java
@@ -0,0 +1,13 @@
+package TaeSuH.HMW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
