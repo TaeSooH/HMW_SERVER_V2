bc48061db2aee9f1d05487a52ffdf3bae014baf0
soohyang136 <jsh515228@gmail.com>
2023-04-22 17:11:06 +0900

    ADD :: JwtAuthenticationFilter

---
+++ src/main/java/TaeSuH/HMW/global/security/jwt/auth/AuthDetails.java
@@ -0,0 +1,50 @@
+package TaeSuH.HMW.global.security.jwt.auth;
+
+import TaeSuH.HMW.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton((GrantedAuthority) () -> user.getAuthority().name());
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/TaeSuH/HMW/global/security/jwt/auth/AuthDetailsService.java
@@ -0,0 +1,20 @@
+package TaeSuH.HMW.global.security.jwt.auth;
+
+import TaeSuH.HMW.domain.user.domain.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> new IllegalArgumentException("user not found"));
+    }
+}
